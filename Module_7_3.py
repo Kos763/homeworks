28476b54e51ae0416fc6b0ea6f761809b4e1a37e
Kos763 <falw992@gmail.com>
2024-11-06 19:50:29 +0300

    Module_7_3.py

--- Module_7_3.py
+++ Module_7_3.py
@@ -5,7 +5,7 @@ class WordsFinder:
     def get_all_words(self):
         all_worlds = {}
         for file_name in self.file_name:
-            with open(file_name, 'r', encoding='utf-8') as file:
+            with open(file_name, 'r+', encoding='utf-8') as file:
                 words = file.read().lower()
                 for puncts in [',', '.', '=', '!', '?', ';', ':', ' - ']:
                     words = words.replace(puncts, '')
@@ -19,18 +19,17 @@ class WordsFinder:
         result = {}
         for file_name, words in all_words.items():
             if word in words:
-                result[file_name] = words.index(word)
+                result[file_name] = words.index(word) + 1
         return result
 
     def count(self, word):
         all_words = self.get_all_words()
         result = {}
         for file_name, words in all_words.items():
-            result[file_name] = words.count(word)
+            result[file_name] = words.count(word.lower())
         return result
 
 finder2 = WordsFinder('test_file.txt')
 print(finder2.get_all_words())
 print(finder2.find('text'))
-print(finder2.count('text'))
-
+print(finder2.count('teXT'))
